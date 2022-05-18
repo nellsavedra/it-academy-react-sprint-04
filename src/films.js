1465d9e13cb6a8529f04e454ab9fbfd2889a631b
nellsavedra <nellsavedra@gmail.com>
2022-05-18 09:08:31 +0200

    Implemented function moviesAverageByCategory() [Exercici 6]

--- src/films.js
+++ src/films.js
@@ -1,54 +1,80 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result = array.map((movie) => movie.director);
-  // console.log('Ex. 1 ->', result);
-  return result;
+	let result = array.map(movie => movie.director);
+
+	// console.log('Ex. 1 ->', result);
+
+	return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
-  let result = array.filter((movie) => movie.director == director);
-  // console.log('Ex. 2 ->', result);
-  return result;
+	let result = array.filter(movie => movie.director == director);
+
+	// console.log('Ex. 2 ->', result);
+
+	return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  let result = getMoviesFromDirector(array, director);
-  // console.log(result);
-  let average = result.reduce((prev, curr) => prev.score + curr.score);
-  average = Number((average / result.length).toFixed(2));
-  // console.log('Ex. 3 ->', average);
-  return average;
+	let result = getMoviesFromDirector(array, director);
+
+	// console.log(result);
+
+	let average = result.reduce((prev, curr) => prev.score + curr.score);
+	average = Number((average / result.length).toFixed(2));
+
+	// console.log('Ex. 3 ->', average);
+
+	return average;
 }
 
 // Exercise 4:  Alphabetic order by title
 function orderAlphabetically(array) {
-  let movieTitles = array.map((movie) => movie.title);
-  movieTitles.sort((prev, curr) => (prev > curr ? 1 : -1));
-  movieTitles = movieTitles.splice(0, 20);
-  return movieTitles;
+	let movieTitles = array.map(movie => movie.title);
+	movieTitles.sort((prev, curr) => (prev > curr ? 1 : -1));
+	movieTitles = movieTitles.splice(0, 20);
+
+	// console.log('Ex. 4 ->', movieTitles);
+
+	return movieTitles;
 }
 
 // Exercise 5: Order by year, ascending
 function orderByYear(array) {
-  const movies = JSON.parse(JSON.stringify(array));
+	const movies = array.map(e => ({ ...e }));
 
-  const moviesOrdered = movies.sort((prev, curr) => {
-    if (prev.year == curr.year) {
-      return prev.title.replace(/\s/g, '').toLowerCase() >
-        curr.title.replace(/\s/g, '').toLowerCase()
-        ? 1
-        : -1;
-    }
-    return prev.year - curr.year;
-  });
-  // console.log(moviesOrdered);
-  return moviesOrdered;
+	const moviesOrdered = movies.sort((prev, curr) => {
+		if (prev.year == curr.year) {
+			return prev.title.replace(/\s/g, "").toLowerCase() > curr.title.replace(/\s/g, "").toLowerCase() ? 1 : -1;
+		}
+		return prev.year - curr.year;
+	});
+
+	// console.log('Ex. 5 ->', moviesOrdered);
+
+	return moviesOrdered;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {}
+function moviesAverageByCategory(array, category) {
+	const moviesCat = array.filter(movie => {
+		return movie.genre.length == 1 && movie.genre.includes(category) == true && movie.score > 1;
+	});
+
+	// console.log(moviesCat);
+
+	let averageGenre = 0;
+	for (let movie of moviesCat) {
+		averageGenre += movie.score;
+	}
+	averageGenre = Number((averageGenre / moviesCat.length).toFixed(2));
+
+	// console.log('Ex. 6 ->', averageGenre);
+
+	return averageGenre;
+}
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {}
@@ -58,15 +84,15 @@ function bestFilmOfYear() {}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = {
-    getAllDirectors,
-    getMoviesFromDirector,
-    moviesAverageOfDirector,
-    orderAlphabetically,
-    orderByYear,
-    moviesAverageByCategory,
-    hoursToMinutes,
-    bestFilmOfYear
-  };
+if (typeof module !== "undefined") {
+	module.exports = {
+		getAllDirectors,
+		getMoviesFromDirector,
+		moviesAverageOfDirector,
+		orderAlphabetically,
+		orderByYear,
+		moviesAverageByCategory,
+		hoursToMinutes,
+		bestFilmOfYear,
+	};
 }
